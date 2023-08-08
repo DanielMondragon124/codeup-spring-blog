bef7ee77a477dc8d6e029e57f04603b86be6c142
daniel mondragon <daniel.mondragon004@GMAIL.COM>
2023-08-08 10:30:35 -0500

    update

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,18 +1,38 @@
 package com.codeup.codeupspringblog.controllers;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
+
+@Controller
 public class HomeController {
 
     @GetMapping("/")
+    @ResponseBody
     public String home() {
         return "This is the landing page!";
     }
-
     @GetMapping("/home")
     public String welcome() {
         return "home";
     }
+    @GetMapping("/rolldice")
+    public String promptUser() {
+        return "RollDice";
+    }
+    @GetMapping("/roll-dice/{n}")
+    public String rollDice(@PathVariable int n, Model model) {
+        Random rand = new Random();
+        int diceRoll = 1 + rand.nextInt(6); // random number between 1 and 6
+        model.addAttribute("diceRoll", diceRoll);
+        model.addAttribute("userGuess", n);
+        model.addAttribute("isCorrect", diceRoll == n);
+        return "Diceresult";
+    }
+
 }
 
--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,22 +1,54 @@
 package com.codeup.codeupspringblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/posts")
 public class PostController {
-    @GetMapping("/posts")
-    public String posts() {
-        return "posts index page";
+    @GetMapping("/all")
+    public String allPosts(Model model) {
+        // Create two sample posts and add them to the list
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post();
+        post1.setTitle("Post 1");
+        post1.setBody("This is the content of post 1.");
+        posts.add(post1);
+
+        Post post2 = new Post();
+        post2.setTitle("Post 2");
+        post2.setBody("This is the content of post 2.");
+        posts.add(post2);
+
+        // Pass the list of posts to the view
+        model.addAttribute("posts", posts);
+
+        return "index";
     }
-    @GetMapping("/posts/{id}")
-    public String viewPosts(@PathVariable int id) {
-        return "view an individual posts";
+
+    @GetMapping("/{id}")
+    public String viewPost(@PathVariable int id, Model model) {
+        // Create a sample post for demonstration
+        Post post = new Post();
+        post.setTitle("Sample Post");
+        post.setBody("This is the content of the sample post.");
+
+        // Pass the post object to the view
+        model.addAttribute("post", post);
+
+        return "show";
     }
-    @GetMapping("/posts/create")
+
+    @GetMapping("/create")
     public String createPostsForm() {
         return "view the form for creating a post";
     }
-    @PostMapping("/posts/create")
+
+    @PostMapping("/create")
     public String createPost() {
         return "create a new post";
     }
